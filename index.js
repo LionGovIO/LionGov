bb04f2bb87f096256d7b5e47c8ae6b8a757b90a7
astro3710 <90355777+astro3710@users.noreply.github.com>
2021-09-25 07:47:27 -0700

    store vote in dynamodb

--- index.js
+++ index.js
@@ -1,17 +1,66 @@
+const { v4: uuidv4 } = require('uuid');
+
 const express = require('express');
 const app = express();
 const http = require('http');
 const server = http.createServer(app);
 
+const AWS = require("aws-sdk");
+AWS.config.update({
+  region: 'us-east-1'
+});
+
+var dynamoDB = new AWS.DynamoDB();
+
+
 app.use(express.static('public'))
 
 // body-parser is deprecated, use below instead: https://stackoverflow.com/questions/66525078/bodyparser-is-deprecated
-app.use(express.urlencoded({extended: true}));
+app.use(express.urlencoded({ extended: true }));
 app.use(express.json()) // To parse the incoming requests with JSON payloads
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
 });
+
+function submitVote(data, callback) {
+  var creationTime = Date.now().toString();
+  var voteId = uuidv4();
+  var voteClass = data.voteClass;
+  var voteValue = data.voteValue;
+
+  var params = {
+    TableName: "Votes",
+    Item: {
+      "VoteId": {
+        S: voteId
+      },
+      "VoteClass": {
+        S: voteClass
+      },
+      "VoteValue": {
+        S: voteValue
+      },
+      "CreationTime": {
+        S: creationTime
+      }
+    },
+    ConditionExpression: 'attribute_not_exists(VoteId)' // prevent someone from overriding the vote
+  };
+
+  dynamoDB.putItem(params, function (err, data) {
+    if (err) {
+      console.log(err);
+      if (err.code == 'ConditionalCheckFailedException') {
+        callback('VoteId is already in use', null);
+      } else {
+        callback(err, null);
+      }
+    } else {
+      callback(null, data);
+    }
+  });
+}
 
 // POST method route
 app.post('/submitvote', function (req, res) {
@@ -22,6 +71,18 @@ app.post('/submitvote', function (req, res) {
 
   console.log('postData!');
   console.log(postData);
+
+
+  // TODO: reject invalid post params
+
+  submitVote(postData, function(err, result) {
+    console.log('submitVote callback!');
+    console.log(err);
+    console.log(result);
+
+    // TODO: if error, alert client in UI
+  });
+
 })
 
 // TODO: don't console.log vote data in long term to protect user privacy
