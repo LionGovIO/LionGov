6d688950c226b5e5e6026ff95fa1d09c06f9f0da
astro3710 <90355777+astro3710@users.noreply.github.com>
2021-09-29 18:21:29 -0700

    wasa wasa wasaapppppp; add view proposals table

--- server.js
+++ server.js
@@ -141,6 +141,123 @@ app.get('/getvoteweight', (req, res) => {
 
 // TODO: show error to client when voting twice
 
+
+
+
+
+
+// this function is privacy-first, does not reveal the full address in the query result
+function privacyProposalQuery(data, callback) {
+    console.log('###### privacyProposalQuery 1 !!!!!!!');
+
+    var params = {
+        // KeyConditionExpression: "ProposalId <> 'coffeezilla'", // TODO: make a better query / scan, just a temp hack
+        // ScanIndexForward: false,
+        TableName: 'Proposals'
+    };
+
+    // TODO: pagination
+
+    dynamoDB.scan(params, function (err, data) {
+        if (err) {
+            console.log(err);
+            // TODO: propagate error to client UI via callback
+        } else {
+            console.log('###### privacyProposalQuery 2 !!!!!!!');
+
+            // console.log('userInfoLock query!');
+            console.log(data);           // successful response
+            console.log(JSON.stringify(data));
+
+            
+
+            // TODO: handle pagination
+            // TODO: consider returning latest 50 votes, and return # of votes if possible
+            // pagination in UI is a bonus feature, ok to show latest 50 votes for now
+
+            var outputItems = [];
+            var items = data['Items'];
+            if (items && items.length >= 1) {
+                for (var i = 0; i < items.length; i++) {
+                    var item = items[i];
+                    console.log(item);
+
+                    // TODO: handle case where desired attributes in item are missing, probably throw an error via callback
+
+                    var proposalId = item.ProposalId.S;
+                    var proposalType = item.ProposalType.S;
+                    var creationTime = item.CreationTime.S;
+                    // var walletAddress = item.WalletAddress.S;
+                    var title = item.Title.S;
+                    var description = item.Description.S;
+
+                    // based on how OpenSea obscures wallet in display in profile page
+                    // var obscuredWalletAddress = walletAddress.substring(0, 6) + '...' + walletAddress.substring(walletAddress.length - 4, walletAddress.length);
+
+                    // do not return the VoteValue! ;)
+                    var outputItem = {
+                        proposalId: proposalId,
+                        proposalType: proposalType,
+                        creationTime: creationTime,
+                        title: title,
+                        description: description
+                    }
+
+                    console.log(outputItem);
+
+                    outputItems.push(outputItem);
+                }
+            }
+
+            // TODO: make API inputs / outputs better, better organization, better names, etc.
+
+            var output = {
+                items: outputItems,
+                count: data['Count'],
+                scannedCount: data['ScannedCount']
+            };
+
+            callback(null, output)
+
+            /*
+            var result = {};
+
+            if (data['Items'] && data['Items'].length >= 1) {
+                var item = data['Items'][0];
+            }
+            */
+
+            // addResult(userResult);
+        }
+    });
+}
+
+
+// semi-private since it doesn't reveal who created the proposal
+// TODO: come up with better / more consisten names for all these APIs!!! ;)
+
+// Wasasa wasasaa wasasaaaaaapp!!!!!!!!!!!!!!!!!
+// This sentence says nothing!!! No code reviews right now yayayayayaaya!!! I am freeeeeeeeee!!!
+// COMMENT ON MY SPACING / NAMING JUST ONCE, I DARE UUUU!!!!!!
+// LOOK AT THE VARIABLES BELOWWWWWWWWWW
+var supercalifragilisticexpialidocious_123_CamelCase_MOOOOOONNNN = "123123123123123123"; // METAKOVAN IS AWESOMEEEEEEEE
+var metakovan = "Crypto is the new America." // 100%
+
+app.get('/privacyproposalquery', function (req, res) {
+    // Retrieve the tag from our URL path
+    console.log('privacyproposalquery!');
+    console.log(req.params);
+    console.log(req.query);
+
+    privacyProposalQuery({}, function (err, output) {
+        console.log('output!');
+        console.log(output);
+
+        res.json(output);
+    });
+});
+
+
 // this function is privacy-first, does not reveal the full address in the query result
 function privacyVoteQuery(data, callback) {
     var voteClass = data.voteClass;
