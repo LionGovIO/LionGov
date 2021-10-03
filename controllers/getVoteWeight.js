77f37532f35445eba1cc3ff918a8e550d2d32fff
Arthur Helfstein Fragoso <arthur@life.net.br>
2021-10-03 15:20:38 -0300

    EditorConfig + Refactor Controllers
    - Refactor server.js to individual controller files
    - EditorConfig setting default soft tabs to size of 2 spaces

---
+++ controllers/getVoteWeight.js
@@ -0,0 +1,20 @@
+
+exports.get = function (req, res) {
+  console.log('getvoteweight!');
+  console.log(req.params);
+  console.log(req.query);
+
+  if (req.query) {
+    var walletAddress = req.query.walletAddress;
+
+    if (walletAddress) {
+      walletAddress = walletAddress.trim().toLowerCase();
+      console.log('walletAddress to get voting points of! ' + walletAddress);
+      Blkchain.getVoteWeight(walletAddress, Date.now()).then(voteWeight => {
+        console.log('voteWeight! ' + voteWeight);
+
+        res.end(JSON.stringify(voteWeight));
+      });
+    }
+  }
+}
