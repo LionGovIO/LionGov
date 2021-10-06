fa44a281e4e774f91cfcc249fa3f6b4b0efba6cd
Arthur Helfstein Fragoso <arthur@life.net.br>
2021-10-06 06:05:06 -0300

    Remove unused constants

--- public/blockchain/constants.js
+++ public/blockchain/constants.js
@@ -15,21 +15,7 @@ module.exports = {
       first_block: 9209224
     }
   },
-  MM_CONTRACT_ADDR_POLYGON : "0x5647fe4281f8f6f01e84bce775ad4b828a7b8927",
-  MM_CONTRACT_ADDR_ETHEREUM : "0x6b4c7a5e3f0b99fcd83e9c089bddd6c7fce5c611",
-  MM_CONTRACT_ADDR_BSC : "0xbf05279f9bf1ce69bbfed670813b7e431142afa4",
-  MM_FIRST_BLOCK_POLYGON: 18760128,
-  MM_FIRST_BLOCK_ETHEREUM: 12694701,
-  MM_FIRST_BLOCK_BSC: 9209224,
   SIGNATURE_MESSAGE : "Session id: SESSIONID\n\nSign your session id with your Polygon Account, this proves to us and to all players that you are the real owner of that address and NFTs. This does not cost any fees."
-
 
 ,
 }
-
-
-
-// export const MM_CONTRACT_ADDR_POLYGON = "0x5647fe4281f8f6f01e84bce775ad4b828a7b8927";
-// export const MM_CONTRACT_ADDR_ETHEREUM = "0x6b4c7a5e3f0b99fcd83e9c089bddd6c7fce5c611";
-// export const MM_CONTRACT_ADDR_BSC = "0xbf05279f9bf1ce69bbfed670813b7e431142afa4";
-// export const SIGNATURE_MESSAGE = "Session id: SESSIONID\n\nSign your session id with your Polygon Account, this proves to us and to all players that you are the real owner of that address and NFTs. This does not cost any fees.";
