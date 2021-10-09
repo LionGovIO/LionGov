2d6bd3e8dd3a09a090e0f410c81d380b92074cab
Arthur Helfstein Fragoso <arthur@life.net.br>
2021-10-09 20:02:06 -0300

    Remove CDN use for now, because of bugs
    More at: https://github.com/webpack/webpack/issues/14449

--- config/webpack.dev.js
+++ config/webpack.dev.js
@@ -8,20 +8,4 @@ module.exports = merge(common, {
     static: './dist',
     historyApiFallback: true
   },
-  externalsType: 'script',
-  externals: {
-    react: ['https://unpkg.com/react@17/umd/react.development.js', 'React'],
-    'react-dom': [
-      'https://unpkg.com/react-dom@17/umd/react-dom.development.js',
-      'ReactDOM',
-    ],
-    'react-router-dom': [
-      'https://unpkg.com/react-router-dom/umd/react-router-dom.min.js',
-      'ReactRouterDOM',
-    ],
-    'react-is': [
-      'https://unpkg.com/react-is@16.8.3/umd/react-is.development.js',
-    "ReactIs"],
-    'babel': ["https://unpkg.com/babel-standalone@6.26.0/babel.js", "Babel"],
-  },
 });
--- config/webpack.prod.js
+++ config/webpack.prod.js
@@ -3,20 +3,4 @@ const common = require('./webpack.common.js');
 
 module.exports = merge(common, {
   mode: 'production',
-  externalsType: 'script',
-  externals: {
-    react: ['https://unpkg.com/react@17/umd/react.production.min.js', 'React'],
-    'react-dom': [
-      'https://unpkg.com/react-dom@17/umd/react-dom.production.min.js',
-      'ReactDOM',
-    ],
-    'react-router-dom': [
-      'https://unpkg.com/react-router-dom/umd/react-router-dom.min.js',
-      'ReactRouterDOM',
-    ],
-    'react-is': [
-      'https://unpkg.com/react-is@16.8.3/umd/react-is.production.min.js',
-    "ReactIs"],
-    'babel': ["https://unpkg.com/babel-standalone@6.26.0/babel.js", "Babel"],
-  },
 });
