0c2b063d2e98a1843d530008610fc23b2707db3d
Arthur Helfstein Fragoso <arthur@life.net.br>
2021-10-11 23:52:11 -0300

    Fix Number

--- src/pages/proposal/Proposal.jsx
+++ src/pages/proposal/Proposal.jsx
@@ -36,8 +36,8 @@ function getVotesTable(currentVoteClass, setList) {
       option_Weight['no'] = 0;
 
       for (const i in votes) {
-        option_Weight[votes[i].voteValue.toLowerCase()] += number(votes[i].voteWeight);
-        totalWeight += number(votes[i].voteWeight);
+        option_Weight[votes[i].voteValue.toLowerCase()] += Number(votes[i].voteWeight);
+        totalWeight += Number(votes[i].voteWeight);
       }
 
       //let sum = result.sum;
