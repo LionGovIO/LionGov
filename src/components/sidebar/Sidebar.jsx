9d6009b4624daad967211f40feceec719a77d486
Arthur Helfstein Fragoso <arthur@life.net.br>
2021-10-12 17:10:29 -0300

    Sidapanel works on mobile now

--- src/components/sidebar/Sidebar.jsx
+++ src/components/sidebar/Sidebar.jsx
@@ -8,6 +8,64 @@ import { folder } from '../../assets/svg/folder'
 import { gear } from '../../assets/svg/gear'
 import { houseDoor } from '../../assets/svg/houseDoor'
 
+window.addEventListener('load', function () {
+
+  /* ===== Responsive Sidepanel ====== */
+  const sidePanelToggler = document.getElementById('sidepanel-toggler');
+  const sidePanel = document.getElementById('app-sidepanel');
+  const sidePanelDrop = document.getElementById('sidepanel-drop');
+  const sidePanelClose = document.getElementById('sidepanel-close');
+
+  window.addEventListener('load', function(){
+  	responsiveSidePanel();
+  });
+
+  window.addEventListener('resize', function(){
+  	responsiveSidePanel();
+  });
+
+
+  function responsiveSidePanel() {
+      let w = window.innerWidth;
+  	if(w >= 1200) {
+  	    // if larger
+  	    //console.log('larger');
+  		sidePanel.classList.remove('sidepanel-hidden');
+  		sidePanel.classList.add('sidepanel-visible');
+
+  	} else {
+  	    // if smaller
+  	    //console.log('smaller');
+  	    sidePanel.classList.remove('sidepanel-visible');
+  		sidePanel.classList.add('sidepanel-hidden');
+  	}
+  };
+
+  sidePanelToggler.addEventListener('click', () => {
+  	if (sidePanel.classList.contains('sidepanel-visible')) {
+  		console.log('visible');
+  		sidePanel.classList.remove('sidepanel-visible');
+  		sidePanel.classList.add('sidepanel-hidden');
+
+  	} else {
+  		console.log('hidden');
+  		sidePanel.classList.remove('sidepanel-hidden');
+  		sidePanel.classList.add('sidepanel-visible');
+  	}
+  });
+
+
+  sidePanelClose.addEventListener('click', (e) => {
+  	e.preventDefault();
+  	sidePanelToggler.click();
+  });
+
+  sidePanelDrop.addEventListener('click', (e) => {
+  	sidePanelToggler.click();
+  });
+
+});
+
 export function Sidebar() {
   return (
     <div id="app-sidepanel" className="app-sidepanel" style={{ zIndex: '1'}}>
